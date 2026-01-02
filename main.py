7d022d7a3bb7cd03c142f05b89ea5e3856c10eea
Isaev Elnur <isaevelnurdev@gmail.com>
2026-01-02 15:46:04 +0500

    Draw whole year

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ def gcd(a, b):
     return a if b == 0 else gcd(b, a % b)
 
 root = Tk()
-window_width = 300
-window_height = 300
+window_width = 1000
+window_height = 1000
 root.title("Git activity")
 root.geometry(f"{window_width}x{window_height}")
 
 
 canvas = Canvas(bg="white", width=window_width, height=window_width)
-canvas.pack(anchor=CENTER, expand=1)
+canvas.pack(fill=BOTH, expand=True)
 
 sq_size = 30
-year = 2025
+year = 2026
 month = 1
-weeks = calendar.monthcalendar(year, month)
-x = 0
-y = 0
-for w in weeks:
-    for day in w:
-        if day != 0:
-            canvas.create_rectangle(x, y, x + sq_size, y + sq_size)
-        y = y + sq_size
-    x = x + sq_size
-    y = 0
+x = 10
+y = 10
+for month in range(1, 13):
+    weeks = calendar.monthcalendar(year, month)
+    for w in weeks:
+        for day in w:
+            if day != 0:
+                canvas.create_rectangle(x, y, x + sq_size, y + sq_size)
+            y = y + sq_size
+        x = x + sq_size
+        y = 10
 
 root.mainloop()
