4598ce618cdb273b4382cfa2df64ef73ce02298c
Isaev Elnur <isaevelnurdev@gmail.com>
2026-01-04 10:09:10 +0500

    Add month name

--- main.py
+++ main.py
@@ -15,18 +15,22 @@ canvas = Canvas(bg="white", width=window_width, height=window_width)
 canvas.pack(fill=BOTH, expand=True)
 
 sq_size = 30
-year = 2026
+gap = 10
+year = 2027
 month = 1
-x = 10
-y = 10
+x = 50
+y = 50
 for month in range(1, 13):
     weeks = calendar.monthcalendar(year, month)
+    month_name = calendar.month_name[month]
+    canvas.create_text(x + (7 * sq_size) / 2, y - 10, text=month_name, font=("Arial", 16))
     for w in weeks:
         for day in w:
             if day != 0:
                 canvas.create_rectangle(x, y, x + sq_size, y + sq_size)
             y = y + sq_size
         x = x + sq_size
-        y = 10
+        y = 50
+    x = x + gap
 
 root.mainloop()
